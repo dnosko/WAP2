ffc7b35e905f454f24fbc1e26f3bddf2f663d01c
Daša Nosková <dasa.noskova@avast.com>
2023-03-28 14:51:05 +0200

    format

--- app/src/components/TopSongs.jsx
+++ app/src/components/TopSongs.jsx
@@ -1,17 +1,16 @@
-import ArrowButton from './ArrowButton';
-import { useAuth } from '../userauth'
+import ArrowButton from "./ArrowButton";
+import { useAuth } from "../userauth";
 
 export function TopSongs(props) {
-    let access_token = useAuth()
-    return (
-        <div className="App">
-            <a href="/welcome">
-            </a>
-            { access_token == null ?  <h1>Not granted</h1> : <h1>Hi</h1> }
-            <div className="bottom">
-                <ArrowButton link="/welcome" direction="left"></ArrowButton>
-                <ArrowButton link="" direction="right"></ArrowButton>
-            </div>
-        </div>
-    )
+  let access_token = useAuth();
+  return (
+    <div className='App'>
+      <a href='/welcome'></a>
+      {access_token == null ? <h1>Not granted</h1> : <h1>Hi</h1>}
+      <div className='bottom'>
+        <ArrowButton link='/welcome' direction='left'></ArrowButton>
+        <ArrowButton link='' direction='right'></ArrowButton>
+      </div>
+    </div>
+  );
 }
--- app/src/components/Welcome.jsx
+++ app/src/components/Welcome.jsx
@@ -1,45 +1,53 @@
-import { React} from 'react';
-import {  useNavigate } from 'react-router-dom';
-import { useAuth } from '../userauth'
-import '../css/App.css'
-import '../css/Welcome.css'
+import { React } from "react";
+import { useNavigate } from "react-router-dom";
+import { useAuth } from "../userauth";
+import "../css/App.css";
+import "../css/Welcome.css";
 
-import earth from "../assets/earth-svgrepo-com.svg"
-import jupiter from "../assets/jupiter-svgrepo-com.svg"
-import mars from "../assets/mars-svgrepo-com.svg"
-import mercury from "../assets/mercury-svgrepo-com.svg"
-import moon from "../assets/moon-svgrepo-com.svg"
-import neptune from "../assets/neptune-svgrepo-com.svg"
-import saturn from "../assets/saturn-svgrepo-com.svg"
-import uranus from "../assets/uranus-svgrepo-com.svg"
-import venus from "../assets/venus-svgrepo-com.svg"
+import earth from "../assets/earth-svgrepo-com.svg";
+import jupiter from "../assets/jupiter-svgrepo-com.svg";
+import mars from "../assets/mars-svgrepo-com.svg";
+import mercury from "../assets/mercury-svgrepo-com.svg";
+import moon from "../assets/moon-svgrepo-com.svg";
+import neptune from "../assets/neptune-svgrepo-com.svg";
+import saturn from "../assets/saturn-svgrepo-com.svg";
+import uranus from "../assets/uranus-svgrepo-com.svg";
+import venus from "../assets/venus-svgrepo-com.svg";
 
 export function Welcome(props) {
-    let access_token = useAuth()
-    
-    let navigate = useNavigate(); 
-    const routeChange = () =>{ 
-        let path = `/TopSongs`; 
-        navigate(path);
-    }
+  let access_token = useAuth();
 
-    document.getElementById("body").style.background = "blueviolet";
-    document.getElementById("body").style.color = 'aliceblue'
+  let navigate = useNavigate();
+  const routeChange = () => {
+    let path = `/TopSongs`;
+    navigate(path);
+  };
 
-    
-    return (
-        <div onClick={routeChange} onDragExit={routeChange} className="App next">
-            {access_token == null? <h1>Access not granted!</h1> :
-            <><div>
-                    <img src={saturn} className="planets saturn "></img>
-                    <img src={earth} className="planets earth"></img>
-                    <img src={moon} className="planets moon"></img>
-                </div><img src={neptune} className="planets neptune"></img><div className="heading">
-                        <img src={jupiter} className="planets jupiter"></img>
-                        <h1>Ready? Let's dive in!</h1>
-                        <img src={mars} className="planets mars"></img>
-                    </div><img src={mercury} className="planets mercury"></img><img src={uranus} className="planets uranus"></img><img src={venus} className="planets venus"></img></>
-            }
-            </div>
-      )
+  document.getElementById("body").style.background = "blueviolet";
+  document.getElementById("body").style.color = "aliceblue";
+
+  return (
+    <div onClick={routeChange} onDragExit={routeChange} className='App next'>
+      {access_token == null ? (
+        <h1>Access not granted!</h1>
+      ) : (
+        <>
+          <div>
+            <img src={saturn} className='planets saturn '></img>
+            <img src={earth} className='planets earth'></img>
+            <img src={moon} className='planets moon'></img>
+          </div>
+          <img src={neptune} className='planets neptune'></img>
+          <div className='heading'>
+            <img src={jupiter} className='planets jupiter'></img>
+            <h1>Ready? Let's dive in!</h1>
+            <img src={mars} className='planets mars'></img>
+          </div>
+          <img src={mercury} className='planets mercury'></img>
+          <img src={uranus} className='planets uranus'></img>
+          <img src={venus} className='planets venus'></img>
+        </>
+      )}
+    </div>
+  );
 }
