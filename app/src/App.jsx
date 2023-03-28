5128b1ad2a7c5c4d91366d76db64c648c807e3f4
Daša Nosková <dasa.noskova@gmail.com>
2023-03-28 14:51:05 +0200

    format

--- app/src/App.jsx
+++ app/src/App.jsx
@@ -1,21 +1,20 @@
-import { React, useState } from 'react';
-import { BrowserRouter, Route, Routes} from 'react-router-dom';
-import LoginPage from './components/LoginPage'
-import { Welcome } from './components/Welcome';
-import {TopSongs}   from './components/TopSongs';
-import './css/App.css'
+import { React, useState } from "react";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import LoginPage from "./components/LoginPage";
+import { Welcome } from "./components/Welcome";
+import { TopSongs } from "./components/TopSongs";
+import "./css/App.css";
 
 function App() {
-
-return (
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LoginPage />} />
-      <Route path="welcome" element={<Welcome  />} />
-      <Route path="TopSongs" element={<TopSongs  />}/>
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path='/' element={<LoginPage />} />
+        <Route path='welcome' element={<Welcome />} />
+        <Route path='TopSongs' element={<TopSongs />} />
       </Routes>
-      </BrowserRouter>
+    </BrowserRouter>
   );
 }
 
-export default App
+export default App;
--- server/server.js
+++ server/server.js
@@ -1,111 +1,126 @@
-require('dotenv').config()
-const express = require('express');
-const cors = require('cors');
-var cookieParser = require('cookie-parser');
-const { default: axios } = require('axios');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+var cookieParser = require("cookie-parser");
+const { default: axios } = require("axios");
 var app = express();
 
+app.use(cors()).use(cookieParser());
 
-app.use(cors()).use(cookieParser())
-
-const app_url = process.env.APP_URL
-const client_id = process.env.CLIENT_ID
-const client_secret = process.env.CLIENT_SECRET
-const redirect_uri = process.env.REDIRECT_URI 
-const authEndpoint = 'https://accounts.spotify.com/authorize?'
-const token_endpoint = 'https://accounts.spotify.com/api/token'
-const scopes = ['user-read-private',
-    'user-read-email',
-    'user-top-read', 'user-read-recently-played'];
+const app_url = process.env.APP_URL;
+const client_id = process.env.CLIENT_ID;
+const client_secret = process.env.CLIENT_SECRET;
+const redirect_uri = process.env.REDIRECT_URI;
+const authEndpoint = "https://accounts.spotify.com/authorize?";
+const token_endpoint = "https://accounts.spotify.com/api/token";
+const scopes = [
+  "user-read-private",
+  "user-read-email",
+  "user-top-read",
+  "user-read-recently-played",
+];
 var header = {
-  'Authorization': 'Basic ' + (new Buffer.from(client_id + ':' + client_secret)).toString('base64'),
-  'Content-Type':'application/x-www-form-urlencoded',
-}
+  Authorization:
+    "Basic " +
+    new Buffer.from(client_id + ":" + client_secret).toString("base64"),
+  "Content-Type": "application/x-www-form-urlencoded",
+};
+
 var access_token, refresh_token;
-var stateKey = 'auth_state';
+var stateKey = "auth_state";
 
 function generateRandomString(length) {
-        const possible = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
-        const characters = Array.from({ length }, () => possible.charAt(Math.floor(Math.random() * possible.length)));
-        return characters.join('');
-    }
+  const possible =
+    "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+  const characters = Array.from({ length }, () =>
+    possible.charAt(Math.floor(Math.random() * possible.length))
+  );
+  return characters.join("");
+}
 
-app.get('/auth', (req, res) => {
+app.get("/auth", (req, res) => {
   var state = generateRandomString(16);
   res.cookie(stateKey, state);
 
   const params = new URLSearchParams({
-    response_type: 'code',
+    response_type: "code",
     client_id: client_id,
     scope: scopes,
     redirect_uri: redirect_uri,
     state: state,
-    show_dialog: false
-  })
+    show_dialog: false,
+  });
 
-  const loginUrl = authEndpoint + params.toString()
-  
+  const loginUrl = authEndpoint + params.toString();
+
   res.redirect(loginUrl);
-})
+});
 
-app.get('/callback', function(req, res) {
-
+app.get("/callback", function (req, res) {
   var code = req.query.code || null;
   var state = req.query.state || null;
   var storedState = req.cookies ? req.cookies[stateKey] : null;
 
   if (state == null || state !== storedState || code == null) {
-    res.redirect(app_url)
+    res.redirect(app_url);
   }
 
   res.clearCookie(stateKey);
-  console.log(header)
+  console.log(header);
 
-  axios.post(token_endpoint, {
-    grant_type: 'authorization_code',
-    code: code,
-    redirect_uri: redirect_uri
-  }, {headers: header}
-  ).then((response) => {
-    console.log(response.data)
-    access_token = response.data.access_token
-    refresh_token = response.data.refresh_token
-    res.redirect(app_url+'welcome')
-
-  }).catch ((err) => {
-    console.log('error: ', err);
-    res.status(500).json({ message: 'Internal server error' });
-  })
-
+  axios
+    .post(
+      token_endpoint,
+      {
+        grant_type: "authorization_code",
+        code: code,
+        redirect_uri: redirect_uri,
+      },
+      { headers: header }
+    )
+    .then((response) => {
+      console.log(response.data);
+      access_token = response.data.access_token;
+      refresh_token = response.data.refresh_token;
+      res.redirect(app_url + "welcome");
+    })
+    .catch((err) => {
+      console.log("error: ", err);
+      res.status(500).json({ message: "Internal server error" });
+    });
 });
 
-app.get('/token', function (req, res) {
-
-  axios.get('http://localhost:3001/refresh').then((response) => {
-    res.json({
-      access_token: response.data.access_token,
-      refresh_token: refresh_token
+app.get("/token", function (req, res) {
+  axios
+    .get("http://localhost:3001/refresh")
+    .then((response) => {
+      res.json({
+        access_token: response.data.access_token,
+        refresh_token: refresh_token,
+      });
     })
-  }).catch ((err) => {
-    console.log('error: ', err);
-    res.status(500).json({ message: 'Internal server error' });
-  })
-  
- })
+    .catch((err) => {
+      console.log("error: ", err);
+      res.status(500).json({ message: "Internal server error" });
+    });
+});
 
-app.get('/refresh', function(req, res) {
-
-  axios.post(token_endpoint, {
-    grant_type: 'refresh_token',
-    refresh_token: refresh_token
-  }, { headers: header })
+app.get("/refresh", function (req, res) {
+  axios
+    .post(
+      token_endpoint,
+      {
+        grant_type: "refresh_token",
+        refresh_token: refresh_token,
+      },
+      { headers: header }
+    )
     .then((response) => {
       access_token = response.data.access_token;
       res.json({
-        access_token: access_token
-        }
-      )
-  });
+        access_token: access_token,
+      });
+    });
 });
 
-app.listen(3001)
+app.listen(3001);
