4e7b163825d4e1faa3e8945abab64e4eb8cb11dd
e-mores <79515730+e-mores@users.noreply.github.com>
2023-04-20 20:10:05 +0200

    Music DNA (#5)
    * WIP: refresh token after expired
    
    * authentication and redirect to login
    
    * refactor
    
    * added token to local storage
    
    * added user style page, no styling
    
    * Changed user style to music dna, now determined with more parameters
    
    * basic page structure and category assignment
    
    * Styling
    
    * added navigation buttons and descriptions
    
    * change considered features
    
    * chart style
    
    * category characteristics
    
    * fix
    
    * file reorg
    
    ---------
    
    Co-authored-by: Eva Moresova <xmores01@stud.fit.vutbr.cz>

--- app/src/pages/ArtistMeterPage.jsx
+++ app/src/pages/ArtistMeterPage.jsx
@@ -24,7 +24,7 @@ export function ArtistMeterPage(props) {
         </>
       <div className='bottom'>
         <ArrowButton link='/topsongs' direction='left'></ArrowButton>
-        <ArrowButton link='/' direction='right'></ArrowButton>
+        <ArrowButton link='/music-dna' direction='right'></ArrowButton>
       </div>
     </div>
 	</Auth>
